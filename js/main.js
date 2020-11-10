ba6e6722a4f59097767ce78d6a0ee9627c156afb
IlariaGalliano <ilaria.galliano7@gmail.com>
2020-11-10 14:39:16 +0100

    vue-todolist
    Js iniziato

--- js/main.js
+++ js/main.js
@@ -6,8 +6,20 @@
 
 const app = new Vue({
   el: '#app',
-
-
+  data: {
+    indexList: 0,
+    message: 'Lista',
+    todos:[
+      'Fare la spesa',
+      'Camminare',
+      'Studiare',
+    ]
+  },
+  methods:{
+    orderList(index){
+      this.indexList = index;
+    },
+  }
 
 
 
